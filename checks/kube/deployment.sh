b97ce13203915e09ec6a6cdaae2697621a900729
leiicamundi <153937047+leiicamundi@users.noreply.github.com>
2024-04-22 15:12:43 +0200

    add deployment checks

---
+++ checks/kube/deployment.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+
+set -o pipefail
+
+# Script to check the status of the deployment
+SCRIPT_NAME=$(basename "$0")
+DIR_NAME=$(dirname "$0")
+LVL_1_SCRIPT_NAME="$DIR_NAME/$SCRIPT_NAME"
+
+# Define default variables
+NAMESPACE=""
+HELM_DEPLOYMENT_NAME="camunda"
+SKIP_CHECK_HELM_DEPLOYMENT=0
+REQUIRED_CONTAINERS=("console" "connector" "web-modeler" "optimize" "zeebe" "zeebe-gateway")
+
+usage() {
+    echo "Usage: $0 [-h] [-n NAMESPACE] [-d HELM_DEPLOYMENT_NAME]"
+    echo "Options:"
+    echo "  -h                              Display this help message"
+    echo "  -n NAMESPACE                    Specify the namespace to use"
+    echo "  -d HELM_DEPLOYMENT_NAME         Specify the name of the helm deployment (default: camunda)"
+    echo "  -l SKIP_CHECK_HELM_DEPLOYMENT   Skip checks of the helm deployment (default: 0)"
+    echo "  -c REQUIRED_CONTAINERS          Specify the list of containers to check (comma-separated)"
+    exit 1
+}
+
+# Parse command line options
+while getopts ":hd:n:c:l" opt; do
+    case ${opt} in
+        h)
+            usage
+            ;;
+        n)
+            NAMESPACE=$OPTARG
+            ;;
+        d)
+            HELM_DEPLOYMENT_NAME=$OPTARG
+            ;;        
+        l)
+            SKIP_CHECK_HELM_DEPLOYMENT=1
+            ;;
+        c)
+            IFS=',' read -ra REQUIRED_CONTAINERS <<< "$OPTARG"
+            ;;
+        \?)
+            echo "Invalid option: $OPTARG" 1>&2
+            usage
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument." 1>&2
+            usage
+            ;;
+    esac
+done
+
+SCRIPT_STATUS_OUTPUT=0
+
+# Check if all required options are provided
+if [ -z "$NAMESPACE" ]; then
+    echo "Error: Missing required options (NAMESPACE)." 1>&2
+    usage
+fi
+
+# required commands
+command -v kubectl >/dev/null 2>&1 || { echo >&2 "Error: kubectl is required but not installed. Please install it (https://kubernetes.io/docs/tasks/tools/). Aborting."; exit 1; }
+
+
+# Helm checks of the deployment
+check_helm_deployment() {
+    local last_deployment
+    last_deployment=$(helm list -n "$NAMESPACE" | grep "$HELM_DEPLOYMENT_NAME" | head -n 1)
+
+    if [[ -n "$last_deployment" ]]; then
+        deployment_status=$(echo "$last_deployment" | awk '{ print $8 }')
+        if [[ "$deployment_status" == "deployed" ]]; then
+            echo "[OK] Last Helm deployment $HELM_DEPLOYMENT_NAME was successful"
+        else
+            echo "[KO] Last Helm deployment $HELM_DEPLOYMENT_NAME was not successful: (status=$deployment_status)" >&2
+            SCRIPT_STATUS_OUTPUT=2
+        fi
+    else
+        echo "[KO] No deployment found for $HELM_DEPLOYMENT_NAME in namespace $NAMESPACE" >&2
+        SCRIPT_STATUS_OUTPUT=3
+    fi
+}
+if [ "$SKIP_CHECK_HELM_DEPLOYMENT" -eq 0 ]; then
+    command -v helm >/dev/null 2>&1 || { echo >&2 "Error: helm is required but not installed. Please install it (https://helm.sh/docs/intro/install/). Aborting."; exit 1; }
+    check_helm_deployment
+fi
+
+# check if any pod is in an unhealthy state in the namespace
+check_unhealthy_pods() {
+    local unhealthy_pods
+    unhealthy_pods=$(kubectl get pods -n "$NAMESPACE" --field-selector=status.phase!=Running --no-headers)
+
+    if [[ -z "$unhealthy_pods" ]]; then
+        echo "[OK] All pods are in an healthy state in namespace $NAMESPACE"
+    else
+        echo "[KO] Pods in unhealthy state in namespace $NAMESPACE:" >&2
+        echo "$unhealthy_pods" >&2
+        SCRIPT_STATUS_OUTPUT=4
+    fi
+}
+check_unhealthy_pods
+
+# check if required containers exist in the pods in the namespace
+check_containers_in_pods() {
+    local required_containers=("${REQUIRED_CONTAINERS[@]}")
+
+    local pods_containers
+    pods_containers=$(kubectl get pods -n "$NAMESPACE" -o jsonpath='{range .items[*]}{.metadata.name}{"\t"}{.spec.containers[*].name}{"\n"}{end}')
+
+    for container in "${required_containers[@]}"; do
+        # Check if the container exists in any pod
+        if ! echo "$pods_containers" | awk -v container="$container" '$0 ~ container { found = 1; exit } END { exit !found }'; then
+            echo "[KO] The following required container is missing in the pods in namespace $NAMESPACE: $container" >&2
+            SCRIPT_STATUS_OUTPUT=5
+        fi
+    done
+}
+check_containers_in_pods
+
+# Check if SCRIPT_STATUS_OUTPUT is not equal to zero
+if [ "$SCRIPT_STATUS_OUTPUT" -ne 0 ]; then
+    echo "[KO] ${LVL_1_SCRIPT_NAME}: At least one of the tests failed (error code: ${SCRIPT_STATUS_OUTPUT})." 1>&2
+    exit $SCRIPT_STATUS_OUTPUT
+else
+    echo "[OK] ${LVL_1_SCRIPT_NAME}: All test passed."
+fi
