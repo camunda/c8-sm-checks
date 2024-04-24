0150b03878839455d1d8572df797a3731dd1c160
Leo J <153937047+leiicamundi@users.noreply.github.com>
2024-04-24 10:23:28 +0200

    feat: adding Zeebe Connectivity and kube Tests (#1)
    also added ci and pre-commit hygiene

--- README.md
+++ README.md
@@ -10,37 +10,147 @@ This project aims to perform a health check of a Kubernetes installation with Ca
 
 - [Directory Structure](#directory-structure)
 - [Usage](#usage)
-  - [Kubernetes Connectivity](#kubernetes-connectivity)
+  - [Kubernetes](#kubernetes-connectivity)
   - [Zeebe Connectivity](#zeebe-connectivity)
 - [License](#license)
 
 The `checks` directory contains scripts for verifying Kubernetes and Zeebe connectivity and configuration. Each script can be executed independently.
 
+**Each script can be executed independently depending on the specific aspect you wish to test.**
+
 ## Usage
 
-### Kubernetes Connectivity
+### Kubernetes
 
-The `connectivity.sh` script in the `checks/kube` directory verifies Kubernetes connectivity and configuration. It checks for the presence of services and ingresses that conform to the required specifications.
+Before using the Kubernetes health check scripts, ensure you have access to Kubernetes with a properly defined `kube config` context.
 
-#### Example Usage:
+For more information on setting up `kube config` context, refer to the [Kubernetes documentation](https://kubernetes.io/docs/reference/kubectl/generated/kubectl_config/kubectl_config_use-context/).
+
+#### Deployment Check (`/checks/kube/deployment.sh`)
+
+##### Description:
+
+This script checks the status of a Helm deployment in the specified namespace.
+It verifies the presence and readiness of required containers within the deployment, depending of your topology you may change required containers.
+
+##### Usage:
 ```bash
-./checks/kube/connectivity.sh -n <NAMESPACE>
+Usage: ./checks/kube/deployment.sh [-h] [-n NAMESPACE] [-d HELM_DEPLOYMENT_NAME] [-l] [-c REQUIRED_CONTAINERS]
+Options:
+  -h                              Display this help message
+  -n NAMESPACE                    Specify the namespace to use
+  -d HELM_DEPLOYMENT_NAME         Specify the name of the helm deployment (default: camunda)
+  -l                              Skip checks of the helm deployment (default: 0)
+  -c REQUIRED_CONTAINERS          Specify the list of containers to check (comma-separated, default: console connector web-modeler optimize zeebe zeebe-gateway)
 ```
+
+##### Example:
+```bash
+./checks/kube/deployment.sh -n camunda-primary -d camunda -c "zeebe,zeebe-gateway,web-modeler"
+```
+
+##### Dependencies:
+
+- `kubectl`: Required for interacting with Kubernetes clusters.
+
+#### Connectivity Check (`/checks/kube/connectivity.sh`)
+
+##### Description:
+
+This script verifies Kubernetes connectivity and associated configuration.
+It checks for the presence of services and ingresses that conform to the required specifications.
+
+##### Usage:
+```bash
+Usage: ./checks/kube/connectivity.sh [-h] [-n NAMESPACE] [-i]
+Options:
+  -h                              Display this help message
+  -n NAMESPACE                    Specify the namespace to use
+  -i                              Skip checks of the ingress class (default: 0)
+```
+
+##### Example:
+```bash
+./checks/kube/connectivity.sh -n camunda-primary
+```
+
+##### Dependencies:
+
+- `kubectl`: Required for interacting with Kubernetes clusters.
+- `helm`: Required for managing Helm deployments.
 
 ### Zeebe Connectivity
 
-The `connectivity.sh` script in the `checks/zeebe` directory verifies connectivity with Zeebe. It checks gRPC and HTTP/2 connectivity with Zeebe and the Zeebe Gateway, as well as access token generation.
+#### Token generation check (`/checks/zeebe/token.sh`)
 
-#### Example Usage:
+##### Description:
+
+This script retrieves an access token from an authorization server using client credentials grant.
+
+##### Usage:
 ```bash
-./checks/zeebe/connectivity.sh -h <ZEEBE_HOST> -p <ZEEBE_PORT> -a <AUTH_SERVER_URL> -i <CLIENT_ID> -s <CLIENT_SECRET> -u <TOKEN_AUDIENCE>
+Usage: ./checks/zeebe/token.sh [-h] [-a AUTH_SERVER_URL] [-i CLIENT_ID] [-s CLIENT_SECRET] [-u TOKEN_AUDIENCE]
+Options:
+  -h                          Display this help message
+  -a AUTH_SERVER_URL          Specify the authorization server URL (e.g.: https://local.distro.ultrawombat.com/auth/realms/camunda-platform/protocol/openid-connect/token)
+  -i CLIENT_ID                Specify the client ID
+  -s CLIENT_SECRET            Specify the client secret
+  -u TOKEN_AUDIENCE           Specify the token audience
+  -k                          Skip TLS verification (insecure mode)
+  -r CACERT                   Specify the path to CA certificate file
+  -j CLIENTCERT               Specify the path to client certificate file
 ```
 
-For more information on specific options for each script, refer to the help sections within the scripts themselves.
+##### Example:
+```bash
+./checks/zeebe/token.sh -a https://local.distro.example.com/auth/realms/camunda-platform/protocol/openid-connect/token -i myclientid -s 0Rn28VrQxGNxowrCWe6wbujwFghO4990 -u zeebe.distro.example.com
+```
+
+##### Dependencies:
+
+- `curl`: Required for making HTTP requests.
+- A registred [[1] application on C8 Identity](#Reference)
+
+#### gRPC zeebe check (`/checks/zeebe/connectivity.sh`)
+
+##### Description:
+
+This script verifies connectivity to a Zeebe instance using HTTP/2 and gRPC protocols. It also checks the status using `zbctl`.
+
+##### Usage:
+```bash
+Usage: ./checks/zeebe/connectivity.sh [-h] [-H ZEEBE_HOST]
+Options:
+  -h                     Display this help message
+  -H ZEEBE_HOST          Specify the Zeebe host (e.g., zeebe.c8.camunda.example.com)
+  -f PROTO_FILE          Specify the path to gateway.proto file or leave empty to download it
+  -k                     Skip TLS verification (insecure mode)
+  -r CACERT              Specify the path to CA certificate file
+  -j CLIENTCERT          Specify the path to Client certificate file
+  -a AUTH_SERVER_URL     Specify the authorization server URL (e.g.: https://local.distro.example.com/auth/realms/camunda-platform/protocol/openid-connect/t
+oken)
+  -i CLIENT_ID           Specify the client ID
+  -s CLIENT_SECRET       Specify the client secret
+  -u TOKEN_AUDIENCE      Specify the token audience
+```
+
+##### Example:
+```bash
+./checks/zeebe/connectivity.sh -a https://local.distro.example.com/auth/realms/camunda-platform/protocol/openid-connect/token -i myclientid -s 0Rn28VrQxGNxowrCWe6wbujwFghO4990 -u zeebe.distro.example.com -H zeebe.local.distro.example.com:443
+```
+
+### Dependencies:
+
+- `curl`: Required for making HTTP requests.
+- `grpcurl`: Required for testing gRPC connectivity.
+- `zbctl`: Required for checking Zeebe status.
+- A registred [[1] application on C8 Identity](#Reference)
+
+## Reference
+
+- [[1] C8: How to register your application on Identity](https://github.com/camunda-community-hub/camunda-8-examples/blob/main/payment-example-process-application/kube/README.md#4-generating-an-m2m-token-for-our-application).
 
 ## License
 
-This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
-```
 
-This structure follows common practices seen in many GitHub repositories, providing an overview, table of contents, directory structure, usage instructions, and license information.
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
--- checks/zeebe/connectivity.sh
+++ checks/zeebe/connectivity.sh
@@ -1,35 +1,48 @@
 #!/bin/bash
 
+set -o pipefail
+
 # Script to verify zeebe connectivity
+SCRIPT_NAME=$(basename "$0")
+DIR_NAME=$(dirname "$0")
+LVL_1_SCRIPT_NAME="$DIR_NAME/$SCRIPT_NAME"
 
 # Define default variables
 ZEEBE_HOST=""
-ZEEBE_PORT=""
 PROTO_FILE=""
 SKIP_TLS_VERIFICATION=""
 EXTRA_FLAGS_CURL=""
 EXTRA_FLAGS_GRPCURL=""
+EXTRA_FLAGS_ZBCTL=""
+EXTRA_FLAGS_TOKEN=""
 CACERT=""
 CLIENTCERT=""
-AUTH_TOKEN=""
+ZEEBE_AUTHORIZATION_SERVER_URL=""
+ZEEBE_CLIENT_ID=""
+ZEEBE_CLIENT_SECRET=""
+ZEEBE_TOKEN_AUDIENCE=""
+ZEEBE_TOKEN_SCOPE="camunda-identity"
 
 # Function to display script usage
 usage() {
-    echo "Usage: $0 [-h] [-H ZEEBE_HOST] [-p ZEEBE_PORT]"
+    echo "Usage: $0 [-h] [-H ZEEBE_HOST]"
     echo "Options:"
     echo "  -h                     Display this help message"
-    echo "  -H ZEEBE_HOST          Specify the Zeebe host (e.g., zeebe.c8.camunda.langleu.de)"
-    echo "  -p ZEEBE_PORT          Specify the Zeebe port (default: 443)"
+    echo "  -H ZEEBE_HOST          Specify the Zeebe host with the port (e.g., zeebe.c8.camunda.example.com:443)"
     echo "  -f PROTO_FILE          Specify the path to gateway.proto file or leave empty to download it"
-    echo "  -t AUTH_TOKEN          Specify the auth token to use (optional)"
     echo "  -k                     Skip TLS verification (insecure mode)"
-    echo "  -ca CACERT             Specify the path to CA certificate file"
-    echo "  -cc CLIENTCERT         Specify the path to Client certificate file"
+    echo "  -r CACERT              Specify the path to CA certificate file"
+    echo "  -j CLIENTCERT          Specify the path to Client certificate file"
+    echo "  -a AUTH_SERVER_URL     Specify the authorization server URL (e.g.: https://local.distro.example.com/auth/realms/camunda-platform/protocol/openid-connect/t
+oken)"
+    echo "  -i CLIENT_ID           Specify the client ID"
+    echo "  -s CLIENT_SECRET       Specify the client secret"
+    echo "  -u TOKEN_AUDIENCE      Specify the token audience"
     exit 1
 }
 
 # Parse command line options
-while getopts ":hH:p:f:k" opt; do
+while getopts ":hH:f:kr:j:a:i:s:u:" opt; do
     case ${opt} in
         h)
             usage
@@ -37,20 +50,29 @@ while getopts ":hH:p:f:k" opt; do
         H)
             ZEEBE_HOST=$OPTARG
             ;;
-        p)
-            ZEEBE_PORT=$OPTARG
-            ;;
         f)
             PROTO_FILE=$OPTARG
             ;;
         k)
             SKIP_TLS_VERIFICATION=true
             ;;
-        ca)
+        r)
             CACERT="$OPTARG"
             ;;
-        cc)
+        j)
             CLIENTCERT="$OPTARG"
+            ;;
+        a)
+            ZEEBE_AUTHORIZATION_SERVER_URL=$OPTARG
+            ;;
+        i)
+            ZEEBE_CLIENT_ID=$OPTARG
+            ;;
+        s)
+            ZEEBE_CLIENT_SECRET=$OPTARG
+            ;;
+        u)
+            ZEEBE_TOKEN_AUDIENCE=$OPTARG
             ;;
         \?)
             echo "Invalid option: $OPTARG" 1>&2
@@ -63,33 +85,66 @@ while getopts ":hH:p:f:k" opt; do
     esac
 done
 
-# Check if Zeebe host is provided, if not, prompt user
+SCRIPT_STATUS_OUTPUT=0
+
+# Check if all required options are provided
 if [ -z "$ZEEBE_HOST" ]; then
-    read -p "Enter Zeebe host: " ZEEBE_HOST
+    echo "Error: Missing one of the required options (list of all required options: ZEEBE_HOST)." 1>&2
+    usage
 fi
 
-# Check if Zeebe port is provided, if not, use default value
-if [ -z "$ZEEBE_PORT" ]; then
-    read -p "Enter Zeebe port: " ZEEBE_PORT
+# Extract host and port from ZEEBE_HOST
+if ! [[ $ZEEBE_HOST =~ ^[^:]+:[0-9]+$ ]]; then
+    echo "Error: Invalid format ZEEBE_HOST=$ZEEBE_HOST. Please provide host and port. Aborting." >&2
+    exit 1
 fi
+
+# pre-check requirements
+command -v curl >/dev/null 2>&1 || { echo >&2 "Error: curl is required but not installed. Please install it. Aborting."; exit 1; }
+command -v grpcurl >/dev/null 2>&1 || { echo >&2 "Error: grpcurl is required but not installed. Please install it (https://github.com/fullstorydev/grpcurl?tab=readme-ov-file#installation). Aborting."; exit 1; }
+command -v zbctl >/dev/null 2>&1 || { echo >&2 "Error: zbctl is required but not installed. Please install it (https://docs.camunda.io/docs/apis-tools/cli-client/). Aborting."; exit 1; }
 
 if [ "$SKIP_TLS_VERIFICATION" = true ]; then
     EXTRA_FLAGS_CURL="-k"
     EXTRA_FLAGS_GRPCURL="-insecure"
+    EXTRA_FLAGS_ZBCTL="--insecure"
+    EXTRA_FLAGS_TOKEN="-k"
 fi
 
 if [ -n "${CACERT}" ]; then
-    EXTRA_FLAGS_CURL+=" -cacert ${CACERT} "
-    EXTRA_FLAGS_GRPCURL+="-cacert ${CACERT}"
+    EXTRA_FLAGS_CURL+=" -cacert \"${CACERT}\" "
+    EXTRA_FLAGS_GRPCURL+=" -cacert \"${CACERT}\" "
+    EXTRA_FLAGS_ZBCTL+=" --authority \"${CACERT}\" "
+    EXTRA_FLAGS_TOKEN+=" -p \"${CACERT}\" "
 fi
 
 if [ -n "${CLIENTCERT}" ]; then
-    EXTRA_FLAGS_CURL+=" -cert ${CLIENTCERT} "
-    EXTRA_FLAGS_GRPCURL+=" -cert ${CLIENTCERT} "
+    EXTRA_FLAGS_CURL+=" -cert \"${CLIENTCERT}\" "
+    EXTRA_FLAGS_GRPCURL+=" -cert \"${CLIENTCERT}\" "
+    EXTRA_FLAGS_ZBCTL+=" --certPath \"${CLIENTCERT}\" "
+    EXTRA_FLAGS_TOKEN+=" -j \"${CLIENTCERT}\" "
 fi
 
-# Check if grpcurl is installed
-command -v grpcurl >/dev/null 2>&1 || { echo >&2 "grpcurl is required but not installed. Please install it (https://github.com/fullstorydev/grpcurl?tab=readme-ov-file#installation). Aborting."; exit 1; }
+# Check if token is needed
+access_token=""
+if [ -n "${ZEEBE_AUTHORIZATION_SERVER_URL}" ] || [ -n "${ZEEBE_CLIENT_ID}" ] || [ -n "${ZEEBE_CLIENT_SECRET}" ] || [ -n "${ZEEBE_TOKEN_AUDIENCE}" ]; then
+    token_command="${DIR_NAME}/token.sh -a \"${ZEEBE_AUTHORIZATION_SERVER_URL}\" -i \"${ZEEBE_CLIENT_ID}\" -s \"${ZEEBE_CLIENT_SECRET}\" -u \"${ZEEBE_TOKEN_AUDIENCE}\" ${EXTRA_FLAGS_TOKEN}"
+    token_output=$(eval "${token_command}")
+    access_token=$(echo "$token_output" | sed -n 's/.*Access Token: \(.*\)/\1/p')
+
+    if [ -n "$access_token" ]; then
+        echo "[OK] Auth token successfuly generated"
+    else
+        echo "[KO] Failed to generate access token: $token_output." 1>&2
+        SCRIPT_STATUS_OUTPUT=2
+    fi
+fi
+
+if [ -n "${access_token}" ]; then
+    EXTRA_FLAGS_CURL+=" -H 'Authorization: Bearer ${access_token}' "
+    EXTRA_FLAGS_GRPCURL+=" -H 'Authorization: Bearer ${access_token}' "
+fi
+
 
 # Check if proto file path is provided, if not, download it
 if [ -z "$PROTO_FILE" ]; then
@@ -99,9 +154,48 @@ if [ -z "$PROTO_FILE" ]; then
 fi
 
 # Check HTTP/2 connectivity
-echo "Checking HTTP/2 connectivity to $ZEEBE_HOST:$ZEEBE_PORT"
-curl -v --http2 $EXTRA_FLAGS_CURL https://$ZEEBE_HOST:$ZEEBE_PORT >/dev/null 2>&1 && echo "[OK] HTTP/2 connectivity" || echo "[KO] HTTP/2 connectivity"
+check_http2(){
+    echo "Checking HTTP/2 connectivity to $ZEEBE_HOST"
+    curl_command="curl -so /dev/null --http2 ${EXTRA_FLAGS_CURL} \"https://$ZEEBE_HOST\""
+    if eval "${curl_command}"; then
+        echo "[OK] HTTP/2 connectivity"
+    else
+        echo "[KO] HTTP/2 connectivity" 1>&2
+        SCRIPT_STATUS_OUTPUT=3
+    fi
+}
+check_http2
 
 # Check gRPC connectivity using grpcurl
-echo "Checking gRPC connectivity to $ZEEBE_HOST:$ZEEBE_PORT"
-grpcurl -proto $PROTO_FILE $EXTRA_FLAGS_GRPCURL $ZEEBE_HOST:$ZEEBE_PORT gateway_protocol.Gateway/Topology >/dev/null 2>&1 && echo "[OK] gRPC connectivity" || echo "[KO] gRPC connectivity"
+check_grpc(){
+    echo "Checking gRPC connectivity to $ZEEBE_HOST"
+    grcp_curl_command="grpcurl ${EXTRA_FLAGS_GRPCURL} -proto \"${PROTO_FILE}\" \"${ZEEBE_HOST}\" gateway_protocol.Gateway/Topology"
+    if eval "${grcp_curl_command}"; then
+        echo "[OK] gRPC connectivity"
+    else
+        echo "[KO] gRPC connectivity" 1>&2
+        SCRIPT_STATUS_OUTPUT=4
+    fi
+}
+check_grpc
+
+# Check zbctl status
+check_zbctl() {
+    echo "Checking zbctl status to $ZEEBE_HOST..."
+    zbctl_command="ZEEBE_TOKEN_SCOPE=${ZEEBE_TOKEN_SCOPE}  zbctl status --address \"${ZEEBE_HOST}\" --authzUrl \"${ZEEBE_AUTHORIZATION_SERVER_URL}\" --clientId \"${ZEEBE_CLIENT_ID}\" --clientSecret \"${ZEEBE_CLIENT_SECRET}\" --audience \"${ZEEBE_TOKEN_AUDIENCE}\" ${EXTRA_FLAGS_ZBCTL}"
+    if eval "${zbctl_command}"; then
+        echo "[OK] zbctl status"
+    else
+        echo "[KO] zbctl status" 1>&2
+        SCRIPT_STATUS_OUTPUT=5
+    fi
+}
+check_zbctl
+
+# Check if SCRIPT_STATUS_OUTPUT is not equal to zero
+if [ "$SCRIPT_STATUS_OUTPUT" -ne 0 ]; then
+    echo "[KO] ${LVL_1_SCRIPT_NAME}: At least one of the tests failed (error code: ${SCRIPT_STATUS_OUTPUT})." 1>&2
+    exit $SCRIPT_STATUS_OUTPUT
+else
+    echo "[OK] ${LVL_1_SCRIPT_NAME}: All test passed."
+fi
